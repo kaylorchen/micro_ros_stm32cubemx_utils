cc6915a7060cb431fa17e1e3c010e840e960a904
kaylorchen <kaylor.chen@qq.com>
2024-12-13 22:06:38 +0800

    Update

---
+++ f407/files/opt/microros_f407/libmicroros.cmake
@@ -0,0 +1,14 @@
+#set(micro_ros_base_path "/opt/microros")
+set(micro_ros_base_path "/home/kaylor/work/stm32/libmicroros-stm32f407-dev/files/opt/microros")
+set(MICRO_ROS_INCLUDE_DIRS ${micro_ros_base_path}/microros_static_library/libmicroros/microros_include)
+include_directories(${MICRO_ROS_INCLUDE_DIRS})
+set(MICRO_ROS_LIB ${micro_ros_base_path}/microros_static_library/libmicroros/libmicroros.a)
+if(NOT MICRO_ROS_TRANSPORT)
+  set(MICRO_ROS_TRANSPORT dma)  
+endif()
+set(MICRO_ROS_SRC
+        ${micro_ros_base_path}/extra_sources/custom_memory_manager.c
+        ${micro_ros_base_path}/extra_sources/microros_allocators.c
+        ${micro_ros_base_path}/extra_sources/microros_time.c
+        ${micro_ros_base_path}/extra_sources/microros_transports/${MICRO_ROS_TRANSPORT}_transport.c
+)
---
+++ f407/files/opt/microros_f407/microros_static_library/libmicroros/microros_include/micro_ros.h
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool cubemx_transport_open(struct uxrCustomTransport *transport);
+bool cubemx_transport_close(struct uxrCustomTransport *transport);
+size_t cubemx_transport_write(struct uxrCustomTransport *transport, uint8_t *buf, size_t len, uint8_t *err);
+size_t cubemx_transport_read(struct uxrCustomTransport *transport, uint8_t *buf, size_t len, int timeout, uint8_t *err);
+void *microros_allocate(size_t size, void *state);
+void microros_deallocate(void *pointer, void *state);
+void *microros_reallocate(void *pointer, size_t size, void *state);
+void *microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void *state);
+#ifdef __cplusplus
+}
+#endif
